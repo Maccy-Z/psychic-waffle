2d795bf94bf07433d47deba31940e25c5e293a04
Max Zhu <your_email@example.com>
2024-07-10 03:33:07 +0100

    Add tsgutils

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from torchsparsegradutils import sparse_solve
-from torchsparsegradutils.utils import linear_cg
+from torchsparsegradutils.utils import linear_cg, bicgstab
 import torch
 
 
@@ -8,7 +8,7 @@ A = torch.randn([10, 10])
 b = torch.randn([10])
 
 
-x = sparse_solve.sparse_generic_solve(A, b)
+x = sparse_solve.sparse_generic_solve(A, b, solve=bicgstab)
 
 b_pred = A @ x
 
