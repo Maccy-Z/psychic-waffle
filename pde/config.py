1a6496c5fef683ad50ccac7b4a9e2319b2fe7a46
Max Zhu <mz406@cam.ac.uk>
2024-12-04 21:26:37 +0000

    Save configs

--- pde/config.py
+++ pde/config.py
@@ -80,7 +80,7 @@ class AdjointConfig:
     jac_mode: JacMode = JacMode.GRAPH
 
     # Linear solver settings
-    lin_mode: LinMode = LinMode.ITERATIVE
+    lin_mode: LinMode = LinMode.DENSE
     maxiter: int = 500
     restart: int = 100
     rtol: float = 1e-4
@@ -88,30 +88,30 @@ class AdjointConfig:
 
     def __post_init__(self):
         self.lin_solve_cfg = {"maxiter": self.maxiter, "restart": self.restart, "rtol": self.rtol}
-        # self.lin_solve_cfg = {
-        #     "config_version": 2,
-        #     "determinism_flag": 0,
-        #     "exception_handling": 1,
-        #
-        #     "solver": {
-        #         "monitor_residual": 1,
-        #         # "print_solve_stats": 1,
-        #         "solver": "PBICGSTAB",
-        #         "convergence": "RELATIVE_INI_CORE",
-        #         "tolerance": 1e-4,
-        #         "max_iters": 25,
-        #         # "gmres_n_restart": 75,
-        #         "preconditioner": {
-        #             # "solver": "NOSOLVER",
-        #             "solver": "AMG",
-        #             "algorithm": "AGGREGATION",
-        #             "selector": "SIZE_2",
-        #             "max_iters": 1,
-        #             "cycle": "V",
-        #             # "max_levels": 5,
-        #         }
-        #     }
-        # }
+        self.lin_solve_cfg = {
+            "config_version": 2,
+            "determinism_flag": 0,
+            "exception_handling": 1,
+
+            "solver": {
+                "monitor_residual": 1,
+                # "print_solve_stats": 1,
+                "solver": "PBICGSTAB",
+                "convergence": "RELATIVE_INI_CORE",
+                "tolerance": 1e-4,
+                "max_iters": 25,
+                # "gmres_n_restart": 75,
+                "preconditioner": {
+                    # "solver": "NOSOLVER",
+                    "solver": "AMG",
+                    "algorithm": "AGGREGATION",
+                    "selector": "SIZE_2",
+                    "max_iters": 1,
+                    "cycle": "V",
+                    # "max_levels": 5,
+                }
+            }
+        }
 
 @dataclass
 class Config:
